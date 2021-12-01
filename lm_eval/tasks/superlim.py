de6644227b9fd6fcfb2d607722829136bd22ae48
evigog <evangeliagog@gmail.com>
2021-12-01 18:00:39 +0100

    update swewsc class after the change of format

--- lm_eval/tasks/superlim.py
+++ lm_eval/tasks/superlim.py
@@ -3,12 +3,13 @@ from ..utils import general_detokenize
 from ..metrics import mean, acc_all, metric_max_over_ground_truths
 import os
 from lm_eval.base import rf
+from lm_eval.base import MultipleChoiceTask
 
 class SweWsc(HFTask):
    VERSION = 0
    DATASET_PATH = "AI-Sweden/SuperLim"
    DATASET_NAME = "SweWsc"
-   DATA_FILES = {"test":"SweWsc/test.jsonl"}
+   DATA_FILES = {"test":"SweWsc/test.csv"}
    USE_AUTH_TOKEN = os.environ['HF_TOKEN']
 
    def has_training_docs(self):
@@ -26,11 +27,11 @@ class SweWsc(HFTask):
    def doc_to_text(self, doc):
     raw_passage = doc["text"]
     # NOTE: HuggingFace span indices are word-based not character-based.
-    pre = " ".join(raw_passage.split()[:doc["challenge"]["location"]["start"]])
-    post = raw_passage[len(pre) + len(doc["challenge"]["text"]) + 1:]
-    passage = general_detokenize(pre + " *{}*".format(doc['challenge']["text"]) + post)
-    noun = doc["responses"][0]["text"] #todo add more responses. now we consider only the first
-    pronoun = doc["challenge"]["text"]
+    pre = " ".join(raw_passage.split()[:doc["span1_begin"]])
+    post = raw_passage[len(pre) + len(doc["span1_text"]) + 1:]
+    passage = general_detokenize(pre + " *{}*".format(doc["span1_text"]) + post)
+    noun = doc["span2_text"]
+    pronoun = doc["span1_text"]
     text = (
             f"Passage: {passage}\n"
             + f"Fråga: I avsnittet ovan, syftar pronomenet \"*{pronoun}*\" på \"*{noun}*\"?\n"
@@ -39,7 +40,7 @@ class SweWsc(HFTask):
     return text
 
    def doc_to_target(self, doc):
-    return " " + yesno(doc['responses'][0]["correct"])
+    return " " + yesno(doc['label'])
 
    def construct_requests(self, doc, ctx):
 
@@ -69,3 +70,70 @@ class SweWsc(HFTask):
         }
 
 
+class SweSat(HFTask, MultipleChoiceTask):
+   VERSION = 0
+   DATASET_PATH = "AI-Sweden/SuperLim"
+   DATASET_NAME = "SweSat"
+   DATA_FILES = {"test":"SweSat/test.csv"}
+   USE_AUTH_TOKEN = os.environ['HF_TOKEN']
+
+   def has_training_docs(self):
+        return False
+
+   def has_validation_docs(self):
+        return False
+
+   def has_test_docs(self):
+        return True
+
+   def fewshot_description(self):
+    return "Which one of the alternatives is the correct definition/synonym?"
+
+   def doc_to_text(self, doc):
+    target_word = doc["target_item"]
+    alternatives_list = [doc[key] for key in ["answer_1", "answer_2", "answer_3", "answer_4", "answer_5"]]
+    alternatives_texts = [entry.split("/")[0] for entry in alternatives_list ]
+    text = (
+            f"Hitta synonym eller definition till: {target_word}\n"
+            + f'Alternativer: {", ".join(str(x) for x in alternatives_texts)}\n'
+            +f'Rätt svar:'
+        )
+    return text
+
+   def doc_to_target(self, doc):
+    alternatives = [doc[key] for key in ["answer_1", "answer_2", "answer_3", "answer_4", "answer_5"]]
+    correct_answer = [alt.split("/")[0] for alt in alternatives if "1" in alt][0]
+    return " " + correct_answer
+
+
+   def construct_requests(self, doc, ctx):
+        lls = [
+            rf.loglikelihood(ctx, " {}".format(choice))[0]
+            for alt.split("/")[0] in doc[key] for key in ["answer_1", "answer_2", "answer_3", "answer_4", "answer_5"]
+        ]
+
+        return lls
+   #to fix
+   def process_results(self, doc, results):
+        gold = doc["gold"]
+
+        acc = 1. if np.argmax(results) == gold else 0.
+        completion_len = np.array([float(len(i)) for i in doc["choices"]])
+        acc_norm = 1. if np.argmax(results / completion_len) == gold else 0.
+
+        return {
+            "acc": acc,
+            "acc_norm": acc_norm,
+        }
+   #to fix
+   def higher_is_better(self):
+        return {
+            "acc": True,
+            "acc_norm": True,
+        }
+   #to fix
+   def aggregation(self):
+        return {
+            "acc": mean,
+            "acc_norm": mean,
+        }
