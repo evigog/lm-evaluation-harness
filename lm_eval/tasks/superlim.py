927a50dc5ab047ac2a0f3ccc012b1ffa701d1099
SeverineVerlinden <severine.verlinden@outlook.be>
2021-12-21 09:27:41 +0100

    converted swefracas to a multitask

--- lm_eval/tasks/superlim.py
+++ lm_eval/tasks/superlim.py
@@ -41,12 +41,12 @@ class SweWsc(HFTask):
     return text
 
    def doc_to_target(self, doc):
-    return ' ja' if doc['label']==1 else ' nej'
+    return ' Ja' if doc['label']==1 else ' Nej'
 
    def construct_requests(self, doc, ctx):
 
-        ll_yes, _ = rf.loglikelihood(ctx, ' ja')
-        ll_no, _ = rf.loglikelihood(ctx, ' nej')
+        ll_yes, _ = rf.loglikelihood(ctx, ' Ja')
+        ll_no, _ = rf.loglikelihood(ctx, ' Nej')
 
         return ll_yes, ll_no
 
@@ -159,7 +159,16 @@ class SweFracas(HFTask, MultipleChoiceTask): #The json contains list and I think
    def fewshot_description(self):
     return "Är svaret på frågan ja, vet ej eller nej givet de givna påståendena?"
 
-   def doc_to_text(self, doc):
+   def _convert_standard(self, doc):
+       query = self.doc_to_query(doc)
+       out_doc = {
+           "query": query,
+           "choices": ['Ja','Vet ej','Nej'],
+           "gold": ['Ja','Vet ej','Nej'].index(doc['svar'])
+       }
+       return out_doc
+
+   def doc_to_query(self, doc):
     # raw_passage = doc["passage"]
     # # NOTE: HuggingFace span indices are word-based not character-based.
     # pre = " ".join(raw_passage.split()[:doc["challenge_begin"]])
@@ -181,35 +190,7 @@ class SweFracas(HFTask, MultipleChoiceTask): #The json contains list and I think
         )
     return text
 
-   def doc_to_target(self, doc):
-    return " " + doc['svar']
+   def doc_to_text(self, doc):
+       return doc["query"]
 
-   def construct_requests(self, doc, ctx):
-
-        lls = [
-           rf.loglikelihood(ctx, " {}".format(choice))[0]
-           for choice in ['Ja','Vet ej','Nej']
-        ]
-        return lls
-
-   def process_results(self, doc, results):
-       gold = ['Ja','Vet ej','Nej'].index(doc['svar'])
-
-       acc = 1. if np.argmax(results) == gold else 0.
-       completion_len = np.array([float(len(i)) for i in ['Ja','Vet ej','Nej']])
-       acc_norm = 1. if np.argmax(results / completion_len) == gold else 0.
-
-       return {
-           "acc": acc
-       }
-
-   def higher_is_better(self):
-        return {
-            "acc": True
-        }
-
-   def aggregation(self):
-        return {
-            "acc": mean
-        }
 
