15c574269c684dc0c6561dfc8c3f18b69cc50feb
SeverineVerlinden <severine.verlinden@outlook.be>
2022-01-07 17:13:53 +0100

    add insight

--- lm_eval/tasks/superlim.py
+++ lm_eval/tasks/superlim.py
@@ -13,6 +13,11 @@ class SweWsc(HFTask):
    DATA_FILES = {"test":"SweWsc/test.csv"}
    #USE_AUTH_TOKEN = os.environ['HF_TOKEN']
 
+   TP=0 #when algorithm choses yes and the answer is yes
+   TN=0
+   FP=0
+   FN=0
+
    def has_training_docs(self):
         return False
 
@@ -23,7 +28,7 @@ class SweWsc(HFTask):
         return True
 
    def fewshot_description(self):
-    return "Stämmer substantivet överens med pronomenet?"
+    return "Stämmer substantivet överens med pronomenet, ja eller nej?"
 
    def doc_to_text(self, doc):
     raw_passage = doc["passage"]
@@ -57,6 +62,15 @@ class SweWsc(HFTask):
 
         acc = 1. if (ll_yes > ll_no) == gold else 0.
 
+        if acc == 1 and ll_yes>ll_no:
+            self.TP+=1
+        if acc == 1 and ll_yes<ll_no:
+            self.TN+=1
+        if acc == 0 and ll_yes>ll_no:
+            self.FP+=1
+        if acc == 0 and ll_yes<ll_no:
+            self.FN+=1
+
         return {
             "acc": acc
         }
@@ -67,6 +81,10 @@ class SweWsc(HFTask):
         }
 
    def aggregation(self):
+        print("TP:"+str(self.TP))
+        print("TN:" + str(self.TN))
+        print("FP:" + str(self.FP))
+        print("FN:"+str(self.FN))
         return {
             "acc": mean
         }
@@ -76,6 +94,13 @@ class SweFracas(HFTask, MultipleChoiceTask):
    DATASET_PATH = "AI-Sweden/SuperLim"
    DATASET_NAME = "SweFracas"
    DATA_FILES = {"test":"SweFracas/swefracas.csv"}
+   T_J=0
+   T_N=0
+   T_V=0
+   F_J =0
+   F_N =0
+   F_V =0
+
    #USE_AUTH_TOKEN = os.environ['HF_TOKEN']
 
    def has_training_docs(self):
@@ -94,8 +119,8 @@ class SweFracas(HFTask, MultipleChoiceTask):
        query = self.doc_to_query(doc)
        out_doc = {
            "query": query,
-           "choices": ['Ja','Vet ej','Nej','Jo'],
-           "gold": ['Ja','Vet ej','Nej','Jo'].index(doc['svar'])
+           "choices": ['Ja','Vet ej','Nej'],
+           "gold": ['Ja','Vet ej','Nej'].index(doc['svar'])
        }
        return out_doc
 
@@ -123,6 +148,49 @@ class SweFracas(HFTask, MultipleChoiceTask):
 
    def doc_to_text(self, doc):
        return doc["query"]
+
+   def process_results(self, doc, results):
+       gold = doc["gold"]
+
+       acc = 1. if np.argmax(results) == gold else 0.
+       completion_len = np.array([float(len(i)) for i in doc["choices"]])
+       acc_norm = 1. if np.argmax(results / completion_len) == gold else 0.
+
+       if np.argmax(results) == 0:
+           if acc == 1:
+               self.T_J += 1
+           else:
+               self.F_J += 1
+       elif np.argmax(results) == 1:
+           if acc == 1:
+               self.T_V += 1
+           else:
+               self.F_V += 1
+       elif np.argmax(results) == 2:
+           if acc == 1:
+               self.T_N += 1
+           else:
+               self.F_N += 1
+
+       return {
+           "acc": acc,
+           "acc_norm": acc_norm,
+       }
+
+   def aggregation(self):
+       print("TJa:" + str(self.T_J))
+       print("FJa:" + str(self.F_J))
+       print("TNej:" + str(self.T_N))
+       print("FNej:" + str(self.F_N))
+       print("Tvetej" + str(self.T_V))
+       print("FVetej:" + str(self.F_V))
+       print("Ja" + str(self.J))
+       print("Vetej:" + str(self.V))
+       print("Nej:" + str(self.N))
+       return {
+           "acc": mean,
+           "acc_norm": mean,
+       }
 
 
 class SweSat(HFTask, MultipleChoiceTask):
